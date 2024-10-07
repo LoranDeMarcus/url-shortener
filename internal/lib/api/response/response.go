33c4c36334f59019451e01549a96f8c69bc199a7
Artyom Kuzikov <akuzikov@sberhealth.ru>
2024-10-07 18:21:19 +0300

    Initial commit

---
+++ internal/lib/api/response/response.go
@@ -0,0 +1,63 @@
+package response
+
+import (
+	"fmt"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+	"strings"
+)
+
+type Response struct {
+	Status string `json:"status"` // Error, ok
+	Error  string `json:"error,omitempty"`
+}
+type ResponseWithAlias struct {
+	Response
+	Alias string `json:"alias,omitempty"`
+}
+
+const (
+	StatusOk    = "OK"
+	StatusError = "Error"
+)
+
+func OK() Response {
+	return Response{
+		Status: StatusOk,
+	}
+}
+
+func Error(msg string) Response {
+	return Response{
+		Status: StatusError,
+		Error:  msg,
+	}
+}
+
+func ResponseOK(w http.ResponseWriter, r *http.Request, alias string) {
+	render.JSON(w, r, ResponseWithAlias{
+		Response: ResponseWithAlias.OK(),
+		Alias:    alias,
+	})
+}
+
+func ValidationError(errs validator.ValidationErrors) Response {
+	var errMsgs []string
+
+	for _, err := range errs {
+		switch err.ActualTag() {
+		case "required":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is a required field", err.Field()))
+		case "url":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not a valid URL", err.Field()))
+		default:
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not valid", err.Field()))
+		}
+	}
+
+	return Response{
+		Status: StatusError,
+		Error:  strings.Join(errMsgs, ", "),
+	}
+}
